6aa6caf36b3614cf0016b3959359a1ce1d78a768
vedant-radke <vradke1234@gmail.com>
2025-09-11 01:03:46 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3000", 
+  origin: "mindlog-in.vercel.app", 
   credentials: true
 }));
 app.use(express.json());
