34f2002115d1a5d5b0309e1d111a4f905c984a84
vedant-radke <vradke1234@gmail.com>
2025-09-14 18:49:56 +0530

    changes in server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const cors = require("cors");
 dotenv.config();
 
 const connectDB = require("./config/db");
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: "https://mindlog-in.vercel.app", 
@@ -18,8 +20,6 @@ const authRoutes = require("./routes/auth.routes");
 const journalRoutes = require("./routes/journal.routes");
 const chatRoutes = require("./routes/chat.routes");
 
-const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
